560a95200892112ea778529734e95f5c4fc84fea
Hanshika Jain <jainhanshika25@gmail.com>
2024-02-20 22:17:23 +0530

    changes made

--- draft.py
+++ draft.py
@@ -1,10 +1,7 @@
 a=int(input("Enter number one"))
 b=int(input("Enter number 2"))
-<<<<<<< HEAD
 print(a+b)
-=======
 if a>3:
     print(a+b)
 else:
     print(a-b)
->>>>>>> a5a7742ce4d4e13a005dc093f03d01134056e616
